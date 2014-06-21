490a2b83e8acc8cbf2dee51d1fd9a7b85a6430b7
Leandro Guillen <leandroguillen@gmail.com>
2014-06-21 21:55:38 +0200

    Algunos ficheros de node

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,18 @@ exports.start = function(req, res){
 	res.render('start');
 };
 
-exports.login = function(req, res){
-	res.render('login');
-};
-
 exports.logout = function(req, res){
 	res.render('logout');
 };
 
 exports.error = function(req, res){
 	res.render('error');
+};
+
+exports.index = function(req, res){
+  res.render('index.html', { title: 'Express' });
+};
+
+exports.login = function(req, res){
+  res.render('login', { title: 'Express' });
 };
