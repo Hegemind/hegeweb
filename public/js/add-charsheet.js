9231e148a0ab9dbddb590722a509357a0eb91ac9
EstDiaz <ed@inelmosur.es>
2014-06-21 16:54:38 +0200

    Uso de objetos como variables de habilidades.

--- public/js/add-charsheet.js
+++ public/js/add-charsheet.js
@@ -1,15 +1,5 @@
-var STR = "";
-var STRmod = "";
-var DEX = "";
-var DEXmod = "";
-var CON = "";
-var CONmod = "";
-var INT = "";
-var INTmod = "";
-var WIS = "";
-var WISmod = "";
-var CHA = "";
-var CHAmod = "";
+var abilitiesValue = {STR:0,DEX:0,CON:0,INT:0,WIS:0,CHA:0}
+var abilitiesMod = {STR:0,DEX:0,CON:0,INT:0,WIS:0,CHA:0}
 
 $('input').change(function(){
 	abilitiesUpdate();
@@ -18,46 +8,48 @@ $('input').change(function(){
 
 function abilitiesUpdate(){
 	$('[name="STR-score"]').change( function () { 
-		STR 	= $(this).val();
-		STRmod	= Math.floor((STR/2)-5);
-		$('[name$="STR-mod"]').val(STRmod);
+		abilitiesValue[STR] = $(this).val();
+		abilitiesMod[STR]	= Math.floor((abilitiesValue[STR]/2)-5);
+		$('[name$="STR-mod"]').val(abilitiesMod[STR]);
 	});
 
 	$('[name="DEX-score"]').blur( function () { 
-		DEX = $(this).val();
-		DEXmod	= Math.floor((DEX/2)-5);
-		$('[name$="DEX-mod"]').val(DEXmod);
+		abilitiesValue[DEX] = $(this).val();
+		abilitiesMod[DEX]	= Math.floor((abilitiesValue[DEX]/2)-5);
+		$('[name$="DEX-mod"]').val(abilitiesMod[DEX]);
 	});
 
 	$('[name="CON-score"]').blur( function () { 
-		CON = $(this).val();
-		CONmod	= Math.floor((CON/2)-5);
-		$('[name$="CON-mod"]').val(CONmod);
+		abilitiesValue[CON] = $(this).val();
+		abilitiesMod[CON]	= Math.floor((abilitiesValue[CON]/2)-5);
+		$('[name$="CON-mod"]').val(abilitiesMod[CON]);
 	});
 
 	$('[name="INT-score"]').blur( function () { 
-		INT = $(this).val();
-		INTmod	= Math.floor((INT/2)-5);
-		$('[name$="INT-mod"]').val(INTmod);
+		abilitiesValue[INT] = $(this).val();
+		abilitiesMod[INT]	= Math.floor((abilitiesValue[INT]/2)-5);
+		$('[name$="INT-mod"]').val(abilitiesMod[INT]);
 	});
 
 	$('[name="WIS-score"]').blur( function () { 
-		WIS = $(this).val();
-		WISmod	= Math.floor((WIS/2)-5);
-		$('[name$="WIS-mod"]').val(WISmod);
+		abilitiesValue[WIS] = $(this).val();
+		abilitiesMod[WIS]	= Math.floor((abilitiesValue[WIS]/2)-5);
+		$('[name$="WIS-mod"]').val(abilitiesMod[WIS]);
 	});
 
 	$('[name="CHA-score"]').blur( function () { 
-		CHA = $(this).val();
-		CHAmod	= Math.floor((CHA/2)-5);
-		$('[name$="CHA-mod"]').val(CHAmod);
+		abilitiesValue[CHA] = $(this).val();
+		abilitiesMod[CHA]	= Math.floor((abilitiesValue[CHA]/2)-5);
+		$('[name$="CHA-mod"]').val(abilitiesMod[CHA]);
 	});
+	console.log('Values: '+abilitiesValue);
+	console.log('Modifiers: '+abilitiesMod);
 }
 
 function skillUpdate(){
 	$('.skill.basic').each( function(){
 		var key = $(this).find('.key').val();
-		var keyV = 
+		var keyV = abilitiesMod[key];
 		$(this).find('[name$=-abil]').val(keyV);
 	});
 }
